5441521e8f7ca9a945124d8ae2b51394b1b1c908
wlstnam <wlstnam99@gmail.com>
2023-09-15 14:36:25 +0900

    수정하기 반영

--- src/components/List.js
+++ src/components/List.js
@@ -1,26 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 
-const List = React.memo(
-  ({
-    id,
-    title,
-    completed,
-    todoData,
-    setTodoData,
-    provided,
-    snapshot,
-    handleClick,
-  }) => {
-    const handleCompleteChange = (id) => {
-      let newTodoData = todoData.map((data) => {
-        if (data.id === id) {
-          data.completed = !data.completed;
-        }
-        return data;
-      });
-      setTodoData(newTodoData);
-    };
+const List = ({
+  id,
+  title,
+  completed,
+  todoData,
+  setTodoData,
+  provided,
+  snapshot,
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(title);
 
+  const handleClick = (id) => {
+    let newTodoData = todoData.filter((data) => data.id !== id);
+    setTodoData(newTodoData);
+    localStorage.setItem("todoData", JSON.stringify(newTodoData));
+  };
+
+  const handleCompleteChange = (id) => {
+    let newTodoData = todoData.map((data) => {
+      if (data.id === id) {
+        data.completed = !data.completed;
+      }
+      return data;
+    });
+    setTodoData(newTodoData);
+    localStorage.setItem("todoData", JSON.stringify(newTodoData));
+  };
+
+  const handleEditChange = (e) => {
+    setEditedTitle(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    let newTodoData = todoData.map((data) => {
+      if (data.id === id) {
+        data.title = editedTitle;
+      }
+      return data;
+    });
+    setTodoData(newTodoData);
+    localStorage.setItem("todoData", JSON.stringify(newTodoData));
+    setIsEditing(false);
+  };
+
+  if (isEditing) {
+    return (
+      <div className="flex items-center justify-between w-full px-4 py-1 my-1 text-gray-600 bg-gray-100 border rounded row">
+        <form onSubmit={handleSubmit}>
+          <input
+            className="w-full px-3 py-2 mr-4 text-gray-500 appearance-none"
+            value={editedTitle}
+            onChange={handleEditChange}
+            autoFocus
+          />
+        </form>
+        <div className="items-center">
+          <button
+            class="px-4 py-2 float-right"
+            onClick={() => setIsEditing(false)}
+            type="button"
+          >
+            x
+          </button>
+          <button
+            onClick={handleSubmit}
+            class="px-4 py-2 float-right"
+            type="submit"
+          >
+            save
+          </button>
+        </div>
+      </div>
+    );
+  } else {
     return (
       <div
         key={id}
@@ -34,8 +88,8 @@ const List = React.memo(
         <div className="items-center">
           <input
             type="checkbox"
-            defaultChecked={completed}
             onChange={() => handleCompleteChange(id)}
+            defaultChecked={completed}
           />{" "}
           <span className={completed ? "line-through" : undefined}>
             {title}
@@ -48,10 +102,16 @@ const List = React.memo(
           >
             x
           </button>
+          <button
+            className="float-right px-4 py-2"
+            onClick={() => setIsEditing(true)}
+          >
+            edit
+          </button>
         </div>
       </div>
     );
   }
-);
+};
 
 export default List;
